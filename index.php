44255e9c1b2de4115c93b85951dffb0cb4db99ed
NirupamaSingh <nirupamainfoexpert@gmail.com>
2019-11-09 09:48:32 -0800

    morge conflict solution

--- index.php
+++ index.php
@@ -15,19 +15,6 @@ include('./includes/header.php');
                 </div>
                 <h4>Hello! let's get started</h4>
                 <h6 class="font-weight-light">Sign in to continue.</h6>
-<<<<<<< HEAD
-                <form class="pt-3">
-                  <form action="login_sessions.php" method="post" id="log">
-                  <div class="form-group">
-                    <input type="email" class="form-control form-control-lg" name="email"
-                    id="exampleInputEmail1" placeholder="Useremail">
-                  </div>
-                  <div class="form-group">
-                    <input type="password" class="form-control form-control-lg" id="exampleInputPassword1" name="aadhar" placeholder="Password">
-                  </div>
-                  <div class="mt-3">
-                    <button type="submit" class="btn btn-gradient-success btn-lg mr-2">Submit</button>
-=======
                 <form class="pt-3" action="login.php" method="post" enctype="multipart/form-data">
                   <div class="form-group">
                     <input type="email" class="form-control form-control-lg" name="user" placeholder="Username" required>
@@ -37,14 +24,13 @@ include('./includes/header.php');
                   </div>
                   <div align="center">
                     <input type="submit" name="login" value="Login" class="btn btn-gradient-success btn-lg">
->>>>>>> 0969c14e6974728662410cb230399ca5cd0520a2
+
                   </div>
                   <div class="my-2 d-flex justify-content-between align-items-center">
                     <br>
                     <a href="#" class="auth-link text-black mx-auto">Forgot password?</a>
                   </div>
                 </form>
-              </form>
               </div>
             </div>
           </div>
